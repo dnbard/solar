b4bd2445cdde124f52ebeb78f0b7b855021f3648
Alex Bardanov <dnbard@gmail.com>
2015-05-12 21:55:47 +0300

    remove unnecesary code from perf service

--- src/core/performance.js
+++ src/core/performance.js
@@ -1,24 +1,8 @@
 var dispatcher = require('./dispatcher'),
-    isOpera = !!window.opera || navigator.userAgent.indexOf(' OPR/') >= 0;
-    // Opera 8.0+ (UA detection to detect Blink/v8-powered Opera)
-    isFirefox = typeof InstallTrigger !== 'undefined';   // Firefox 1.0+
-    isSafari = Object.prototype.toString.call(window.HTMLElement).indexOf('Constructor') > 0;
-    // At least Safari 3+: "[object HTMLElementConstructor]"
-    isChrome = !!window.chrome && !isOpera;              // Chrome 1+
-    isIE = /*@cc_on!@*/false || !!document.documentMode; // At least IE6;
+    isChrome = !!window.chrome && !isOpera;
 
 exports.calculate = function(){
-    var lastFrame = new Date();
-
-    dispatcher.register(function(payload){
-        var currentFrame = new Date();
-
-        if (payload.action !== 'frame'){ return; }
-
-        if (!isChrome){
-            setTimeout(function(){
-                dispatcher.dispatch({ action: 'frame-stop' });
-            }, 0);
-        }
-    });
+    if (!isChrome){
+        dispatcher.dispatch({ action: 'frame-stop' });
+    }
 }
