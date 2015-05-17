690d8e2d118c029b5f14ef0cf4f1f612c22b816b
Alex Bardanov <dnbard@gmail.com>
2015-05-17 21:29:37 +0300

    gh-pages initial commit

--- src/core/performance.js
+++
@@ -1,9 +0,0 @@
-var dispatcher = require('./dispatcher'),
-    isOpera = !!window.opera || navigator.userAgent.indexOf(' OPR/') >= 0,
-    isChrome = !!window.chrome && !isOpera;
-
-exports.calculate = function(){
-    if (!isChrome){
-        dispatcher.dispatch({ action: 'frame-stop' });
-    }
-}
